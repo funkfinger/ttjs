2ab937d6c0806b725d6be4733a9f1498c1d3f361
Jay Wiggins <jay@irk.com>
2015-06-18 06:04:28 -0700

    working on testing and codeship integration

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,7 @@ var karma = require('karma').server;
 var mocha = require('gulp-mocha');
 
 
-/**
- * Run test once and exit
- */
-gulp.task('test', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done);
-});
+gulp.task('test', ['karma', 'mocha']);
 
 gulp.task('watch-mocha', function() {
   gulp.watch(['src/server/**'], ['mocha']);
@@ -19,6 +11,16 @@ gulp.task('watch-mocha', function() {
 
 gulp.task('mocha', function(done){
   return gulp.src(['src/server/__tests__/*.js'], { read: false })
-  .pipe(mocha({ reporter: 'list' }));
-    // .on('error', gutil.log);
+  .pipe(mocha({ reporter: 'list' }))
+  .once('end', function () {
+    process.exit();
+  });
+  // .on('error', gutil.log);
+});
+
+gulp.task('karma', function(done){
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);  
 });
