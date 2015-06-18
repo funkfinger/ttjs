c241503ef1401bd166b8dbf0ecf8de8a198bd450
Jay Wiggins <jay@irk.com>
2015-06-18 09:30:18 -0700

    working on heroku deploy

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,5 @@ var app = module.exports = express();
 app.get('/', function(req, res){
   res.end('Hello World');
 });
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('running...');
