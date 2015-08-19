4cd60b09a8c5c569a4f4845fb96d5acdced4632c
Jay Wiggins <jay@irk.com>
2015-08-18 23:36:59 -0700

    created at and updated at dates on access log

--- src/server/__tests__/access-log-test.js
+++ src/server/__tests__/access-log-test.js
@@ -3,6 +3,16 @@ var AccessLog = db.AccessLog;
 
 describe('access log model tests', function(done) {
 
+  it('should have a created and updated at date', function() {
+    var al = new AccessLog({data: 'data'})
+    return al.saveAsync()
+      .then(function(newAl){
+        assert.ok(newAl[0].createdAt);
+        assert.ok(newAl[0].updatedAt);
+        return assert.equal(newAl[0].createdAt, newAl[0].updatedAt);
+      })
+  });
+
   it('should exist', function() {
     var al = new AccessLog({data: 'data'});
     assert.equal(al.data, 'data');
--- src/server/models/access-log.js
+++ src/server/models/access-log.js
@@ -1,7 +1,18 @@
 var mongoose = require('mongoose');
 
 var accessLogSchema = new mongoose.Schema( {
-  data: { type: String, required: true }
+  data: { type: String, required: true },
+  createdAt: { type: Date },
+  updatedAt: { type: Date }
+});
+
+accessLogSchema.pre('save', function(next){
+  now = new Date();
+  this.updatedAt = now;
+  if ( !this.createdAt ) {
+    this.createdAt = now;
+  }
+  next();
 });
 
 var AccessLog = mongoose.model('AccessLog', accessLogSchema);
