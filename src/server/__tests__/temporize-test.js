f222f71abbf00b92c88ff63ee697b6c0eb754b93
Jay Wiggins <jay@irk.com>
2015-11-01 11:18:56 -0700

    codeship debug

--- src/server/__tests__/temporize-test.js
+++ src/server/__tests__/temporize-test.js
@@ -45,7 +45,7 @@ describe('temporize tests', function() {
   })
   
 
-  it('should be able to schedule something with temporize', function() {
+  it.only('should be able to schedule something with temporize', function() {
     
     helper.nock(process.env.TEMPORIZE_URL)
       .post('/v1/events/20151107T080000Z/http%3A%2F%2Fgoogle.com')
